8c341c33a89bac082fafc039c236ee82ae3c32d9
DyylanCelentano <dcelentano@fi.uba.ar>
2025-05-12 03:54:34 -0300

    Mejora de pagina y agrego de carrusel

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-import './slider.css'
+import './style.css'
+
+const carrusel = document.getElementById('carrusel')
+let pos_horizontal = 0
+const velocidad = 3 // Pixeles por frame
+
+function movimientoCarrusel() {
+    pos_horizontal += velocidad
+    if ( pos_horizontal >= carrusel.scrollWidth / 1.5 ){
+        pos_horizontal = 0
+    }
+    carrusel.style.transform = `translateX(-${pos_horizontal}px)`
+    requestAnimationFrame(movimientoCarrusel)
+}
+movimientoCarrusel()
